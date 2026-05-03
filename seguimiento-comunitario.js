3ed2918b7137d7069f17b6415934b456fc5dc6a9
Nelson Diaz <nelsonacm9211@gmail.com>
2026-05-03 11:58:53 -0500

    Modicicación de seguimiento

---
+++ seguimiento-comunitario.js
@@ -0,0 +1,14 @@
+let semana = 1
+const semanasDeSeguimiento = 6
+let propuestasAtendidas = 0
+
+console.log("Seguimiento comunitario semanal")
+
+while (semana <= semanasDeSeguimiento) {
+    console.log("Semana de seguimiento número:", semana)
+    propuestasAtendidas = propuestasAtendidas + 2
+    console.log("Porpuestas atendidas hasta ahora:", propuestasAtendidas)
+    semana = semana + 1
+}
+console.log("Seguimiento comunitario finalizado.")
+console.log("Total de propuestas atendidas:", propuestasAtendidas)
