e4b32a1d6bef9e0a5c2557156784112f89aeac24
Nelson Diaz <nelsonacm9211@gmail.com>
2026-05-03 11:41:52 -0500

    Modicicación de cantidad de ciclos

--- ciclo-while.js
+++ ciclo-while.js
@@ -1,12 +1,18 @@
 let intento = 1
-const maximoIntentos = 3
+let ciclo = 1
+const maximoIntentos = 30000
+
+if (maximoIntentos < 1 ) {
+  console.log("El número máximo de intentos debe ser menor que 1.")
+}
 
 console.log("Práctica inicial con while")
 console.log("Vamos a repetir un proceso mientras el intento sea menor o igual al máximo.")
 
 while (intento <= maximoIntentos) {
-  console.log("Intento de revisión número:", intento)
+  console.log("Intento de revisión número:", intento, "ciclo número")
   intento = intento + 1
+  ciclo = ciclo + 1
 }
 
 console.log("Proceso de revisión finalizado.")
