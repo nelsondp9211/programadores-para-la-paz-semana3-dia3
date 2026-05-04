378cd77c2558a79ccc31ee7d3a02b062bb74f9f0
Nelson Diaz <nelsonacm9211@gmail.com>
2026-05-04 18:35:48 -0500

    clase arrays.js

---
+++ Repaso-while.js
@@ -0,0 +1,7 @@
+let intento = 1; 
+const MAX_INTENTOS = 5;
+
+while (intento <= MAX_INTENTOS) {
+    console.log("Intento" + intento);
+    intento = intento + 1; 
+}
---
+++ arrays.js
@@ -0,0 +1,9 @@
+let frutas = ["manzana", "pera", "banana"];
+
+
+
+for (let fruta of frutas) {
+    console.log(fruta);
+}
+
+console.log(frutas.length);
